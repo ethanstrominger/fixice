ed191a35ec136ea221696264185bb3bf4bc5e395
Ethan Strominger <ethanstrominger2@gmail.com>
2026-02-26 20:22:56 -0500

    Modify coffee, adjust photo

---
+++ dist/coffee-archives.js
@@ -0,0 +1,7 @@
+// Archive list for coffee.html
+// To archive: 1) Copy coffee.html to coffee-YY-M-DD.html
+//             2) Add an entry below
+var COFFEE_ARCHIVES = [
+  { file: "coffee.html", date: "26-3-4", title: "North Shore LUCE/Immigrant Orgs" },
+  { file: "coffee-26-2-18.html", date: "26-2-18", title: "North Shore LUCE/Immigrant Orgs" },
+];
